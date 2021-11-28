a97acf3d216f04f90f450d66bc0a6927d8e306bc
esiale <blazejp0@gmail.com>
2021-11-28 20:03:44 +0100

    Add color palettes, add styling to error modal

--- src/components/ApiErrorNotification.js
+++ src/components/ApiErrorNotification.js
@@ -1,5 +1,6 @@
 import useApiError from '../common/hooks/useApiError';
 import Portal from './Portal';
+import alertIcon from '../images/alert.png';
 import { useEffect, useRef } from 'react';
 import { Transition } from '@headlessui/react';
 
@@ -12,7 +13,7 @@ const ApiErrorNotification = () => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       removeError();
-    }, 2000);
+    }, 3000);
     return () => clearTimeout(timeout);
   });
 
@@ -24,7 +25,7 @@ const ApiErrorNotification = () => {
     <Portal>
       <Transition
         show={!!error}
-        class="fixed top-5 left-2/4 transform -translate-x-1/2"
+        class="w-max max-w-sm sm:max-w-max rounded fixed top-5 left-2/4 transform -translate-x-1/2 flex flex-col justify-center items-center shadow"
         enter="transition-opacity duration-300"
         enterFrom="opacity-0"
         enterTo="opacity-100"
@@ -32,8 +33,17 @@ const ApiErrorNotification = () => {
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        {error ?? errorRef.current}
-        <button onClick={handleClick}>Close Modal</button>
+        <div className="flex items-center self-start bg-red-500 w-full px-4 py-1.5 text-white font-bold rounded-t gap-2">
+          <img className="w-8 h-8" src={alertIcon} alt="Warning icon" />
+          An error has occured!
+        </div>
+        <div className="px-4 py-4 text-center">{error ?? errorRef.current}</div>
+        <button
+          className="w-full px-4 py-1.5 bg-gray-100"
+          onClick={handleClick}
+        >
+          Dismiss
+        </button>
       </Transition>
     </Portal>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,52 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      lato: 'lato',
+    },
+    extend: {
+      colors: {
+        'spun-pearl': {
+          DEFAULT: '#AAABBC',
+          50: '#FFFFFF',
+          100: '#FFFFFF',
+          200: '#EEEFF2',
+          300: '#D8D8E0',
+          400: '#C1C2CE',
+          500: '#AAABBC',
+          600: '#8B8CA3',
+          700: '#6C6E89',
+          800: '#54556A',
+          900: '#3B3C4B',
+        },
+        'ship-cove': {
+          DEFAULT: '#6C91C2',
+          50: '#EEF2F8',
+          100: '#DFE7F2',
+          200: '#C3D2E6',
+          300: '#A6BCDA',
+          400: '#89A7CE',
+          500: '#6C91C2',
+          600: '#4874AE',
+          700: '#385986',
+          800: '#273F5E',
+          900: '#172537',
+        },
+        'natural-gray': {
+          DEFAULT: '#8B8982',
+          50: '#E3E3E1',
+          100: '#DAD9D7',
+          200: '#C6C5C1',
+          300: '#B2B1AC',
+          400: '#9F9D97',
+          500: '#8B8982',
+          600: '#6E6D66',
+          700: '#51504B',
+          800: '#343330',
+          900: '#171715',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
