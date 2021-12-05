040da5480a517b4f1c2de295baacab8db52e74cd
esiale <blazejp0@gmail.com>
2021-12-05 14:49:25 +0100

    Add redirect from '/' to posts

--- src/routes/secureRoutes.js
+++ src/routes/secureRoutes.js
@@ -1,7 +1,9 @@
 import Posts from '../pages/Posts';
 import Users from '../pages/Users';
+import { Navigate } from 'react-router-dom';
 
 const secureRoutes = [
+  { path: '/', element: <Navigate replace to="/posts" /> },
   { path: '/posts/*', element: <Posts /> },
   { path: '/users/*', element: <Users /> },
 ];
